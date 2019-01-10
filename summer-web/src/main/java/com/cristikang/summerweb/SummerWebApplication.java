911f8cc06a344a27d15015de065c11511b130aa5
sunkang <sunkang@shein.com>
2019-01-10 10:35:33 +0800

    架构

---
+++ summer-web/src/main/java/com/cristikang/summerweb/SummerWebApplication.java
@@ -0,0 +1,16 @@
+package com.cristikang.summerweb;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SummerWebApplication {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(SummerWebApplication.class, args);
+    }
+
+}
+
